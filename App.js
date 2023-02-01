222f5de02106d248e95ea2554c4c4980caefb4a8
christianb3ll <christian.b311@gmail.com>
2023-02-01 17:33:42 +0000

    Add basic stack navigation

--- App.js
+++ App.js
@@ -1,12 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Dashboard page
+function DashboardPage({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>This is the dashboard</Text>
+
+      <TouchableOpacity onPress={() => navigation.navigate("Study")}>
+          <Text>Study</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => navigation.navigate("Review")}>
+          <Text>Review</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => navigation.navigate("My Kanji")}>
+          <Text>My Kanji</Text>
+      </TouchableOpacity>
+    </View>
+    );
+  };
+
+// Study page
+function StudyPage({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>This is the Study page</Text>
+    </View>
+    );
+  };
+
+// Review page
+function ReviewPage({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>This is the Review page</Text>
+    </View>
+    );
+  };
+
+// MyKanji page
+function MyKanjiPage({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>This is MyKanji page</Text>
+    </View>
+    );
+  };
+
+// Create the navigation stack
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Dashboard" options={{headerShown: false}} component={DashboardPage} />
+        <Stack.Screen name="Study" options={{headerShown: false}} component={StudyPage} />
+        <Stack.Screen name="Review" options={{headerShown: false}} component={ReviewPage} />
+        <Stack.Screen name="My Kanji" options={{headerShown: false}} component={MyKanjiPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
